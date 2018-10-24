a71e00357a37bd90da5d05211c9fd2a672390ac5
Paul Clark <clark.paul123@gmail.com>
2018-10-24 10:31:49 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,32 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    animal_array = []
+    arguments.each do |country|
+      self.each do |animal, country2|
+        if country == country2
+          animal_array << animal
+        end
+      end
+    end
+    animal_array
   end
 end
+
+# not sure how line 5 works...
+# splat (*) allows you to pass any number of arguments into a method
+#below without self also works 
+=begin
+class Hash
+  def keys_of(*arguments)
+    animal_array = []
+    arguments.each do |country|
+      each do |animal, country2|
+        if country == country2
+          animal_array << animal
+        end
+      end
+    end
+    animal_array
+  end
+end
+=end
